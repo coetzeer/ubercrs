45b10943d4d272c2d30750e584d38999d63ab640
coetzeer <coetzeer@gmail.com>
2014-06-30 22:45:52 +0100

    Hooray for git!

---
+++ manifests/site.pp
@@ -0,0 +1,41 @@
+include baseconfig
+
+node 'rcs-common' {
+
+
+}
+
+node 'gitolite' {
+
+
+}
+
+node 'gitlab' {
+
+
+}
+
+node 'gitorious' {
+
+
+}
+
+node 'reviewboard' {
+
+
+}
+
+node 'cvs' {
+
+
+}
+
+node 'svn' {
+
+
+}
+
+node 'hg' {
+
+
+}
---
+++ provision.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -e
+
+#rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
+            
+function install_module {
+   if [ ! -d /etc/puppet/modules/$1 ];
+    then
+        puppet module install $2 --modulepath /etc/puppet/modules
+    fi
+}
+
+function add_host {
+	echo "checking hosts file for $1"
+	EXISTS=`grep $2 /etc/hosts | wc -l`
+	if [ $EXISTS -eq 0 ];then
+		echo "$2    $1.coetzee.com    $1" >> /etc/hosts
+	else
+		echo "host $1 exists"
+	fi
+}
+ 
+install_module mysql puppetlabs-mysql
+install_module apache puppetlabs-apache
+install_module puppetdb puppetlabs-puppetdb
+install_module dashboard puppetlabs-dashboard
+add_host gitolite 192.168.0.28
+add_host gitlab 192.168.0.22
+add_host gitorious 192.168.0.23
+add_host reviewboard 192.168.0.24
+add_host cvs 192.168.0.21       
+add_host svn 192.168.0.26
+add_host hg 192.168.0.27
+
+add_host puppet 192.168.0.31
+add_host master1 192.168.0.32
+add_host master2 192.168.0.33
+add_host cacert1 192.168.0.34
+add_host cacert2 192.168.0.35       
+add_host puppetdb-postgres 192.168.0.36
+add_host puppetdb 192.168.0.37
+add_host dashboard 192.168.0.38
+      

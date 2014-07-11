33e1200f64d1a2390a8483fd468e4799bca3b93e
coetzeer@gmail.com <coetzeer@gmail.com>
2014-07-11 17:54:14 +0100

    setting up users

--- manifests/site.pp
+++ manifests/site.pp
@@ -59,4 +59,22 @@ node 'hg' {
 
   class { 'mercurial': }
 
+  ini_setting { 'mercurial_ini':
+    ensure  => present,
+    path    => '/etc/mercurial/hgrc',
+    section => 'trusted',
+    setting => 'groups',
+    value   => 'phabricator',
+    require => Class['mercurial']
+  }
+
+  ini_setting { 'mercurial_ini':
+    ensure  => present,
+    path    => '/etc/mercurial/hgrc',
+    section => 'trusted',
+    setting => 'users',
+    value   => 'apache,vcs-user,phd-user',
+    require => Class['mercurial']
+  }
+
 }
